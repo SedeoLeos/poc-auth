368f6374aab87b900f5e81d53fe66147da71e1a0
Slaega <88974755+SedeoLeos@users.noreply.github.com>
2024-11-21 10:03:01 +0100

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,24 @@
+'use client'
 import Image from "next/image";
+import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const getMe = async () => {
+    return fetch('http://localhost:3333/api/v1.0/users/me',{
+      credentials: 'include', 
+    })
+  }
+  const { data, setData } = useState(null)
+  useEffect(() => {
+    getMe().then((value) => {
+      value.json().then((subData) => {
+        console.log('value', subData)
+      })
+    })
+  })
+
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -24,9 +42,9 @@ export default function Home() {
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
+          <Link
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            href="/login"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -37,16 +55,16 @@ export default function Home() {
               width={20}
               height={20}
             />
-            Deploy now
-          </a>
-          <a
+            Login
+          </Link>
+          <Link
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            href="/otp"
             target="_blank"
             rel="noopener noreferrer"
           >
             Read our docs
-          </a>
+          </Link>
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
